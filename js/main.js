75edeb301a42fbf662fda171cf097f75de7d918d
Lmao5 <cilipadi270@gmail.com>
2020-09-25 20:37:28 +0800

    Minor Shift

--- js/main.js
+++ js/main.js
@@ -11,3 +11,25 @@ if ('serviceWorker' in navigator){
 //     var fullName = document.getElementById("fullName")
 //     alert(fullName)
 // }
+
+var app = document.getElementById('typewriter');
+
+    var typewriter = new Typewriter(app, {
+      loop: true
+    });
+
+    typewriter.typeString("Hello there!<br>I am Hasan")
+      .pauseFor(2500)
+      .deleteAll()
+      .typeString('New to Python')
+      .pauseFor(2500)
+      .deleteChars(6)
+      .typeString('HTML')
+      .pauseFor(2500)
+      .deleteChars(4)
+      .typeString('Javascript')
+      .pauseFor(2500)
+      .deleteChars(10)
+      .typeString('CSS')
+      .pauseFor(2500)
+      .start();
